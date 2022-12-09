63fa85f380387e84a01dabfe0e2e7461a95dba47
Julia Sherbinina <sherbinina953@gmail.com>
2022-12-09 19:27:29 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {
+	$('.header__burger').on('click', function () {
+		$('.header__burger, .header__list').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+
+
+
+	const slider = $('.slider').slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		speed: 1000,
+		// autoplay: true,
+		autoplaySpeed: 3000,
+		touchThreshold: 8,
+		responsive: [
+			{
+				breakpoint: 1050,
+				settings: {
+					dots: true,
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					dots: true,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+				}
+			},
+		]
+	});
+
+
+	slider.on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+		$(`.products__slider-nav[data-slide="${currentSlide}"]`).removeClass('active');
+		$(`.products__slider-nav[data-slide="${nextSlide}"]`).addClass('active');
+	});
+
+	$('.products__slider-nav').on('click', (event) => {
+		slider.slick('slickGoTo', event.target.attributes['data-slide'].value);
+	});
+
+	$("a").on('click', function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('.header__burger, .header__list').removeClass('active');
+			$('body').removeClass('lock');
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 100, function () {
+				window.location.hash = hash;
+			});
+		}
+	});
+
+	$('#order-form').validate({
+		rules: {
+			userName: {
+				required: true,
+				minlength: 2
+			},
+			userPhone: 'required'
+		},
+		messages: {
+			userName: {
+				required: "Введіть своє ім'я",
+				minlength: jQuery.validator.format("Введіть мінімум {0} символа")
+			},
+			userPhone: 'Введіть номер телефону',
+		}
+	});
+
+	$("input[name=userPhone]").mask("+38(999) 999-99-99");
+
+	$('form.form').submit(function(e) {
+		e.preventDefault();
+		if (!$(this).valid()) {
+			return;
+		}
+		$.ajax({
+			type: 'POST',
+			url: 'mailer/smart.php',
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+
+
+			$('form.form').trigger('reset');
+		});
+		return false;
+	});
+});
---
+++ mailer/smart.php
@@ -0,0 +1,37 @@
+<?php 
+
+$name = $_POST['userName'];
+$phone = $_POST['userPhone'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = '2002julia247681';                 // Наш логин
+$mail->Password = 'tcldfhajtybcdqmd';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('2002julia247681@gmail.com', 'BakeCake');   // От кого письмо 
+$mail->addAddress('2002julia247681@gmail.com');     // Add a recipient
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone;
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
